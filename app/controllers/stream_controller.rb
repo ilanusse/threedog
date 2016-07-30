6d6646405b81e6d000748d68cb1b6c8413c19dd7
Inaki <inaki.lanusse@wolox.com.ar>
2016-07-30 12:05:50 -0300

    Testing Redis pubsub

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -2,12 +2,26 @@ class StreamController < ApplicationController
   include ActionController::Live
 
   def stream_listen
+    return head 404 unless params[:id]
     response.headers['Content-Type'] = 'text/event-stream'
-    10.times {
-      response.stream.write 'Testing.'
-      sleep(1.0/24.0)
-    }
+    # blocks the current thread
+    # Stream is on a background thread and will remain open until redis.psubscribe exits.
+    redis = Redis.new
+    redis.psubscribe("#{params[:id]}:*") do |on|
+      on.pmessage do |pattern, event, data|
+        response.stream.write("Event: #{event}\n")
+        response.stream.write("Data: #{data}\n\n")
+      end
+    end
+  rescue IOError
+    # Client disconnect
   ensure
+    redis.quit
     response.stream.close
+  end
+
+  def broadcast
+    redis = Redis.new
+    redis.publish("#{params[:id]}:#{params[:packet_number]}", 'Test')
   end
 end
